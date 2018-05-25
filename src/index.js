f85f4aa7996e64b797e47d7f0dfac4715a557762
BellaAguirre <yunoshe1@gmail.com>
2018-05-24 19:47:35 -0500

    insertando funciones encode y decode

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ let botonDecifrar = document.getElementById('decifrador');
 
 botonCifrar.addEventListener('click', function(e){
     e.preventDefault();
-    debugger
     descifrado.innerHTML = window.cipher.encode(offset.value,string.value);
 });
 botonDecifrar.addEventListener('click', function(e){
     e.preventDefault();
-    descifrado.value = window.cipher.decode(offset.value,string.value);
+    descifrado.innerHTML = window.cipher.decode(offset.value,string.value);
 });
