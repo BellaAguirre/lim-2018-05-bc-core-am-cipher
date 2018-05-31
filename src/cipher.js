5d71680608b7f92823912ad4117cf60cfff5d999
BellaAguirre <yunoshe1@gmail.com>
2018-05-31 10:24:07 -0500

    implementando test

--- src/cipher.js
+++ src/cipher.js
@@ -1,36 +1,36 @@
 window.cipher = {
   // ... 
-  encode: (offset, string)=>{
-   return cipher.refactorFor(offset, string);
+  encode: (offset, string) => {// funcion para ejecutar el cifrado
+    return cipher.refactorFor(offset, string);// retorna el resultado de cifrar
   },
 
-  decode:(offset, string) =>{
-  offset=(26-(offset%26))%26;
-  return cipher.refactorFor(offset,string);
+  decode: (offset, string) => {// funcion para jecutar el descifrado
+    offset = (26 - (offset % 26)) % 26;// recalculando un nuevo offset
+    return cipher.refactorFor(offset, string);// retorna el resultado de descifrar
   },
-  refactorFor:(offset, string) => {
-    let resultado="";  
-    for(let i=0;i<string.length;i++){
-      let caracter1 = string.charCodeAt(i);
-      if(caracter1>=65 && caracter1<=90){
-        resultado+=String.fromCharCode((caracter1-65 +parseInt(offset))% 26 +65);
-    }else if(caracter1>=97 && caracter1<=122){
-        resultado+=String.fromCharCode((caracter1-97 +parseInt(offset))% 26 + 97);
-    }else{
-        resultado+=String.fromCharCode(caracter1);
+  refactorFor:(offset, string) => {// funcion para hacer el cifrado y descifrado
+    let resultado = "";
+    offset = parseInt(offset);  
+    for (let i = 0; i < string.length; i++){// este for recorre la cantidad de cadena del string
+      let caracter1 = string.charCodeAt(i);// recibiendo el valor de un caracter en la posicion i
+      if (caracter1 >= 65 && caracter1 <= 90){// evaluando si el valor esta entre 65 y 90 en el codigo ASCII(mayusculas)
+        resultado += String.fromCharCode((caracter1 - 65 + offset) % 26 + 65);// si esta añadimos a nuestro resultado
+      } else if (caracter1 >= 97 && caracter1 <= 122){// evaluando si el valor esta entre 97 y 122 en el codigo ASCII (minusculas)
+        resultado += String.fromCharCode((caracter1 - 97 + offset) % 26 + 97);// si esta añadimos a nuestro resultado
+      } else {
+        resultado += String.fromCharCode(caracter1);// caso contrario solo añadimos la letra de este valor 
     }
   }
-  return resultado;
+    return resultado;// retornamos el resultado obtenido
   },
-  createCipherWithOffset:(offset)=>{
-   const result={
-     encode(string){
-      return cipher.encode(offset,string);
-     },
-    decode(string){
-      return cipher.decode(offset,string);
-     }
-     
+  createCipherWithOffset: (offset) => {
+    const result = {
+      encode(string) {
+         return cipher.encode(offset, string);
+      },
+      decode(string) {
+         return cipher.decode(offset, string);
+      }
     }
     return result;
   }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-let offset = document.getElementById('desplazar');
-let string = document.getElementById('mensaje');
-let descifrado = document.getElementById('resultado');
+let offset = document.getElementById('desplazar');//variable para el offset
+let string = document.getElementById('mensaje');//variable para el string ingresado
+let resultFinal = document.getElementById('resultado');//varible para mostrar el resultado de cifrar o descifrar
+let botonCifrar = document.getElementById('cifrador');//variable para el boton cifrar
+let botonDecifrar = document.getElementById('decifrador');//variable para el boton descifrar
+let botonSorpresa = document.getElementById('cipher');
+let sorpresa =document.getElementById('sorpresa');
 
-let botonCifrar = document.getElementById('cifrador');
-let botonDecifrar = document.getElementById('decifrador');
-
-botonCifrar.addEventListener('click', function(e){
+botonCifrar.addEventListener('click', function(e){//evento click para cifrar un mensaje
     e.preventDefault();
-    descifrado.innerHTML="";
-    descifrado.innerHTML = window.cipher.encode(offset.value,string.value);
+    resultFinal.innerHTML = window.cipher.encode(offset.value, string.value);//mostrando el resultado de cifrar en el html
 });
-botonDecifrar.addEventListener('click', function(e){
+botonDecifrar.addEventListener('click', function(e){//evento click para descifrar un mensaje
     e.preventDefault();
-    descifrado.innerHTML="";
-    descifrado.innerHTML = window.cipher.decode(offset.value,string.value);
+    resultFinal.innerHTML = window.cipher.decode(offset.value, string.value);//mostrando el resultado de descifrar en el html
+});
+botonSorpresa.addEventListener('click', function(e){
+    e.preventDefault();
+    sorpresa.innerHTML = window.cipher.createCipherWithOffset(offset.value).encode(string.value);
 });
