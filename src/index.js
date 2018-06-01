4c87d78c0658c8fa9178130a99d5bd2c7de083b6
BellaAguirre <yunoshe1@gmail.com>
2018-06-01 12:51:08 -0500

    mejorando estilos

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
 let offset = document.getElementById('desplazar');//variable para el offset
 let string = document.getElementById('mensaje');//variable para el string ingresado
-let resultFinal = document.getElementById('resultado');//varible para mostrar el resultado de cifrar o descifrar
+let resultFinal;//varible para mostrar el resultado de cifrar o descifrar
+
+//botones de inicio
+let btnCifra = document.getElementById('cifrar');
+let btnDescifra = document.getElementById('descifra');
+//form
+let formPregunta = document.getElementById('containerMenu');
+let formCifrador = document.getElementById('container');
+let btnVolver = document.getElementById('volver');
+
+// Botones dentro del formulario
 let botonCifrar = document.getElementById('cifrador');//variable para el boton cifrar
 let botonDecifrar = document.getElementById('decifrador');//variable para el boton descifrar
 
+
+formCifrador.style.display = 'none';
+btnCifra.addEventListener('click', function(e){
+    e.preventDefault();
+    formPregunta.style.display='none';
+    formCifrador.style.display='block';
+    botonDecifrar.style.display='none';
+});
+
+btnDescifra.addEventListener('click',function(e){
+    e.preventDefault();
+    formPregunta.style.display='none';
+    formCifrador.style.display='block';
+    botonCifrar.style.display='none';
+    botonDecifrar.style.display = 'block';
+});
+btnVolver.addEventListener('click', function(e){
+    formPregunta.style.display = 'block';
+    formCifrador.style.display = 'none';
+});
 botonCifrar.addEventListener('click', function(e){//evento click para cifrar un mensaje
     e.preventDefault();
     //mostrando el resultado de cifrar en el html
-    resultFinal.innerHTML = window.cipher.encode(offset.value, string.value);
+    resultFinal= window.cipher.encode(offset.value, string.value);
+    string.value=resultFinal;
     
 });
 botonDecifrar.addEventListener('click', function(e){//evento click para descifrar un mensaje
     e.preventDefault();
-    resultFinal.innerHTML = window.cipher.decode(offset.value, string.value);//mostrando el resultado de descifrar en el html
+    
+    resultFinal = window.cipher.decode(offset.value, string.value);//mostrando el resultado de descifrar en el html
+    string.value=resultFinal;
 });
