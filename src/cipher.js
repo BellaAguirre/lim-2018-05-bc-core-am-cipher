496f3166548fa8fb35c2aeb5516deafdcd298222
BellaAguirre <yunoshe1@gmail.com>
2018-06-07 10:07:08 -0500

    cambios del feedback

--- src/cipher.js
+++ src/cipher.js
@@ -1,39 +1,32 @@
 window.cipher = {
-  // ... 
-  encode: (offset, string) => {// funcion para ejecutar el cifrado
-    return cipher.refactorFor(offset, string);// retorna el resultado de cifrar
-  },
-
-  decode: (offset, string) => {// funcion para jecutar el descifrado
+  // funcion de cifrar
+  encode: (offset, string) => cipher.refactorFor(offset, string),
+  decode: (offset, string) => {
     offset = (26 - (offset % 26)) % 26;// recalculando un nuevo offset
-    return cipher.refactorFor(offset, string);// retorna el resultado de descifrar
+    return cipher.refactorFor(offset, string);
   },
-  refactorFor:(offset, string) => {// funcion para hacer el cifrado y descifrado
+  refactorFor:(offset, string) => {
+    // funcion para el recorrido del for
     let resultado = "";
     offset = parseInt(offset);  
-    for (let i = 0; i < string.length; i++){// este for recorre la cantidad de cadena del string
-      let caracter1 = string.charCodeAt(i);// recibiendo el valor de un caracter en la posicion i
+    for (let i = 0; i < string.length; i++){
+      let caracter1 = string.charCodeAt(i);
       if (caracter1 >= 65 && caracter1 <= 90){// evaluando si el valor esta entre 65 y 90 en el codigo ASCII(mayusculas)
-        resultado += String.fromCharCode((caracter1 - 65 + offset) % 26 + 65);// si esta añadimos a nuestro resultado
+        resultado += String.fromCharCode((caracter1 - 65 + offset) % 26 + 65);
       } else if (caracter1 >= 97 && caracter1 <= 122){// evaluando si el valor esta entre 97 y 122 en el codigo ASCII (minusculas)
-        resultado += String.fromCharCode((caracter1 - 97 + offset) % 26 + 97);// si esta añadimos a nuestro resultado
+        resultado += String.fromCharCode((caracter1 - 97 + offset) % 26 + 97);
       } else {
         resultado += String.fromCharCode(caracter1);// caso contrario solo añadimos la letra de este valor 
     }
   }
-    return resultado;// retornamos el resultado obtenido
+    return resultado;
   },
   //funcion del hacking edition retorna un objeto con dos funciones
   createCipherWithOffset: (offset) => {
     const result = {
-      encode(string) {
-         return cipher.encode(offset, string);
-      },
-      decode(string) {
-         return cipher.decode(offset, string);
-      }
+      encode(string) {return cipher.encode(offset, string);},
+      decode(string) {return cipher.decode(offset, string);}
     }
     return result;
   }
 };
- 
--- src/index.js
+++ src/index.js
@@ -1,72 +1,81 @@
-let offset = document.getElementById('desplazar');//variable para el offset
-let string = document.getElementById('mensaje');//variable para el string ingresado
+//variables offset y string
+let offset = document.getElementById('desplazar');
+let string = document.getElementById('mensaje');
 let resultFinal;//varible para mostrar el resultado de cifrar o descifrar
 //botones de inicio
 let btnCifra = document.getElementById('cifrar');
 let btnDescifra = document.getElementById('descifra');
 //form
-let formPregunta = document.getElementById('containerMenu');
+let formPregunta = document.getElementById('container_menu');
 let formCifrador = document.getElementById('container');
 // Botones dentro del formulario
-let botonCifrar = document.getElementById('cifrador');//variable para el boton cifrar
-let botonDecifrar = document.getElementById('decifrador');//variable para el boton descifrar
+let botonCifrar = document.getElementById('cifrador');
+let botonDecifrar = document.getElementById('decifrador');
 let btnVolver = document.getElementById('volver');
+//variables para mensaje error
+let mensajeError = document.getElementById('mensaje_error');
+let errorText = document.getElementById('error_text');
 
-formCifrador.style.display = 'none';
 //Evento click de los botones del menu
-btnCifra.addEventListener('click', function(e){
-    e.preventDefault();
-    formPregunta.style.display = 'none';
-    formCifrador.style.display = 'block';
-    botonDecifrar.style.display = 'none';
+btnCifra.addEventListener('click', () => {
+    formPregunta.classList.remove('block');
+    formPregunta.classList.add('none');
+    formCifrador.classList.remove('none');
+    formCifrador.classList.add('block');
+    botonDecifrar.classList.remove('block');
+    botonDecifrar.classList.add('none');
+    botonCifrar.classList.add('block');
 });
 
-btnDescifra.addEventListener('click', function(e){
-    e.preventDefault();
-    formPregunta.style.display = 'none';
-    formCifrador.style.display = 'block';
-    botonCifrar.style.display = 'none';
-    botonDecifrar.style.display = 'inline block';
+btnDescifra.addEventListener('click', () => {
+    formPregunta.classList.remove('block');
+    formPregunta.classList.add('none');
+    formCifrador.classList.remove('none');
+    formCifrador.classList.add('block');
+    botonCifrar.classList.remove('block');
+    botonCifrar.classList.add('none');
+    botonDecifrar.classList.add('block');
 });
 //Evento click del boton de volver
-btnVolver.addEventListener('click', function(){
-    formPregunta.style.display = 'block';
-    formCifrador.style.display = 'none';
+btnVolver.addEventListener('click', () => {
+    formPregunta.classList.remove('none');
+    formPregunta.classList.add('block');
+    formCifrador.classList.remove('block');
+    formCifrador.classList.add('none');
 });
 //evento click para cifrar un mensaje
 botonCifrar.addEventListener('click', function(e){
     e.preventDefault();
     //validando espacios vacios
     if(offset.value === '') {
-        document.getElementById('mensajeError').innerHTML = 'Ingresa tu clave';
-        document.getElementById('errorText').innerHTML = '';
+        mensajeError.innerHTML = 'Ingresa tu clave';
+        errorText.innerHTML = '';
     } else if(string.value.trim() === '') {
-        document.getElementById('mensajeError').innerHTML = '';
-        document.getElementById('errorText').innerHTML = 'ingresa un mensaje';
+        mensajeError.innerHTML = '';
+        errorText.innerHTML = 'ingresa un mensaje';
     }else{
         //mostrando el resultado de cifrar en el html
-        document.getElementById('mensajeError').innerHTML = '';
-        document.getElementById('errorText').innerHTML = '';
+        mensajeError.innerHTML = '';
+        errorText.innerHTML = '';
         resultFinal = window.cipher.encode(offset.value, string.value);
         string.value = resultFinal;
         document.forms['myform']['mensaje'].readOnly = true;
     }
-    
 });
 //evento click para descifrar un mensaje
 botonDecifrar.addEventListener('click', function(e){
     e.preventDefault();
     //validando espacios vacios
     if(offset.value === '') {
-        document.getElementById('mensajeError').innerHTML = 'Ingresa tu clave';
-        document.getElementById('errorText').innerHTML = '';
+       mensajeError.innerHTML = 'Ingresa tu clave';
+        errorText.innerHTML = '';
     } else if(string.value.trim() === '') {
-        document.getElementById('mensajeError').innerHTML = '';
-        document.getElementById('errorText').innerHTML = 'ingresa un mensaje';
+        mensajeError.innerHTML = '';
+        errorText.innerHTML = 'ingresa un mensaje';
     }else{
         //mostrando el resultado de descifrar en el html
-        document.getElementById('mensajeError').innerHTML = '';
-        document.getElementById('errorText').innerHTML = '';
+        mensajeError.innerHTML = '';
+        errorText.innerHTML = '';
         resultFinal = window.cipher.decode(offset.value, string.value);
         string.value = resultFinal;
         document.forms['myform']['mensaje'].readOnly = true;//bloquear textarea para no ingresar nada despues de descifrar
